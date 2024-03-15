cf2e55ea9d23f4203dd1dc7c14cf15f5f7cfc144
felipelopesreis <felipe.reis2@estudante.ifms.edu.br>
2024-03-14 21:28:30 -0400

    portifolio

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { Avatar } from "./components/Avatar"
+
+
+function App() {
+
+
+  return (
+    <>
+    <section className="flex justify-center mt-4"> 
+      <Avatar/>
+    </section>
+    </>
+  )
+}
+
+export default App
