948ac3474ce98bcf801dfdf394fbeeacd3ef805b
felipelopesreis <felipe.reis2@estudante.ifms.edu.br>
2024-04-03 22:35:52 -0400

    estilizando-portifolio

--- src/App.tsx
+++ src/App.tsx
@@ -6,8 +6,30 @@ function App() {
 
   return (
     <>
-    <section className="flex justify-center mt-4"> 
-      <Avatar/>
+    <section className="flex justify-center mt-4 "> 
+    
+      
+      <div className="m-20 tracking-[.25em]">
+      <h1 className="text-2xl m-8 text-center">Bem Vindo! Eu sou o Felipe</h1>
+
+      
+      
+      <p className="whitespace-pre"> Explore meu trabalho como programador júnior e descubra como posso contribuir para o sucesso do seu projeto.</p>
+      <p className="text-center">Desenvolvendo o futuro, um código de cada vez.</p>
+
+
+    <div className="mt-6 flex justify-center">
+        <Avatar/>
+    </div>
+
+
+    <div className="flex justify-center itens-center gap-18 mt-24">
+    </div>
+    </div>
+
+    
+
+
     </section>
     </>
   )
--- src/components/Avatar.tsx
+++ src/components/Avatar.tsx
@@ -1,7 +1,7 @@
 export function Avatar(){
     return(
         <div className="avatar">
-  <div className="w-24 rounded-full ring ring-primary ring-offset-base-100 ring-offset-2">
+  <div className="w-44 rounded-full ring ring-primary ring-offset-base-100 ring-offset-2">
     <img src="https://s3.amazonaws.com/comicgeeks/characters/avatars/37872.jpg" />
   </div>
 </div>
