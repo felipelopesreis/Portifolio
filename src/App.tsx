bece41de0bc0196ca0b987e903575b4b07cdbc9b
felipelopesreis <felipe.reis2@estudante.ifms.edu.br>
2024-04-04 21:57:20 -0400

    portifolio-adcProjetos

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { Avatar } from "./components/Avatar"
+import { ThemeController } from "./components/themeController"
 
 
 function App() {
@@ -20,11 +21,37 @@ function App() {
 
     <div className="mt-6 flex justify-center">
         <Avatar/>
+        
     </div>
 
+    <div className="mt-6 flex justify-center" >
+      <ThemeController/>
+    </div>  
 
-    <div className="flex justify-center itens-center gap-18 mt-24">
+    <div className="text-center ">
+      <p className="p-10 ">Projetos desenvolvidos</p>
     </div>
+
+    <div className="flex first-line">
+      
+    <img className="float-left w-64 h-64" src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/51/Pokebola-pokeball-png-0.png/800px-Pokebola-pokeball-png-0.png"/>
+      <p>Maybe we can live without libraries, </p>
+
+
+      <img className="float-left w-64 h-64" src="https://i.pinimg.com/564x/00/2e/69/002e69110468629d0a56c17473e061c5.jpg"/>
+      <p>Maybe we can live without libraries, </p>
+
+
+      <img className="float-right w-64 h-64" src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/51/Pokebola-pokeball-png-0.png/800px-Pokebola-pokeball-png-0.png"/>
+      <p>Maybe we can live without libraries, people like you and me</p>
+  
+      <img className="float-right w-64 h-64" src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/51/Pokebola-pokeball-png-0.png/800px-Pokebola-pokeball-png-0.png"/>
+      <p>Maybe we can live without libraries, people like you and me</p>
+
+
+</div>
+
+    
     </div>
 
     
